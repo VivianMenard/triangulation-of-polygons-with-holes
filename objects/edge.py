4ca7aa9c0c28e1508868d1f1c03aee4bfa4d6dd7
vivian menard <menardvivian@gmail.com>
2024-11-07 23:24:28 +0100

    fix imports

--- objects/edge.py
+++ objects/edge.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from objects.vertex import Vertex
 
 
--- objects/polygon.py
+++ objects/polygon.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from objects.edge import Edge
 from objects.vertex import Vertex
 
