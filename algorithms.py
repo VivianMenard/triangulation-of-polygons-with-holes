0107ff9d74ae61615edca21473543d256d04d2f6
vivian menard <menardvivian@gmail.com>
2024-11-23 20:30:05 +0100

    add make_monotone_mountains that transform trapezoids into monotone mountains

--- algorithms.py
+++ algorithms.py
@@ -1,12 +1,20 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from random import shuffle
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
-from objects import Node, Trapezoid
+from objects import (
+    Edge,
+    MonotoneMountain,
+    MonotoneVertex,
+    Node,
+    Trapezoid,
+    Vertex,
+)
 
 if TYPE_CHECKING:
-    from objects import Edge, Polygon, Vertex
+    from objects import Polygon
 
 
 def trapezoidation(polygon: Polygon) -> Node:
@@ -32,3 +40,57 @@ def trapezoidation(polygon: Polygon) -> Node:
         start_node.insert_edge(edge, top_should_be_inserted, bottom_should_be_inserted)
 
     return search_tree
+
+
+def select_inside_trapezoids(all_trapezoids: list[Trapezoid]) -> list[Trapezoid]:
+    return [trap for trap in all_trapezoids if trap.is_inside]
+
+
+def make_monotone_mountains(
+    trapezoids: list[Trapezoid],
+) -> list[MonotoneMountain]:
+    above_vertex_by_base_edge: dict[Edge, dict[Vertex, Vertex]] = defaultdict(dict)
+
+    for trap in trapezoids:
+        mountain_bases: list[Edge] = []
+
+        for edge in [trap.left_edge, trap.right_edge]:
+            edge = cast(Edge, edge)
+            if (
+                trap.bottom_vertex != edge.bottom_vertex
+                or trap.top_vertex != edge.top_vertex
+            ):
+                mountain_bases.append(edge)
+
+        assert len(mountain_bases) > 0
+
+        for mountain_base in mountain_bases:
+            above_vertex_by_base_edge[mountain_base][
+                cast(Vertex, trap.bottom_vertex)
+            ] = cast(Vertex, trap.top_vertex)
+
+    monotone_mountains: list[MonotoneMountain] = []
+
+    for base, above_vertex_mapping in above_vertex_by_base_edge.items():
+        below_monotone_vertex: MonotoneVertex | None = None
+        current_vertex = base.bottom_vertex
+        monotone_mountain_created = False
+
+        while current_vertex is not None:
+            current_monotone_vertex = MonotoneVertex(
+                vertex=current_vertex, below=below_monotone_vertex
+            )
+            if below_monotone_vertex:
+                below_monotone_vertex.above = current_monotone_vertex
+
+            above_vertex = above_vertex_mapping.get(current_vertex, None)
+            current_vertex = above_vertex
+            below_monotone_vertex = current_monotone_vertex
+
+            if not monotone_mountain_created:
+                monotone_mountains.append(
+                    MonotoneMountain(current_monotone_vertex, base)
+                )
+                monotone_mountain_created = True
+
+    return monotone_mountains
