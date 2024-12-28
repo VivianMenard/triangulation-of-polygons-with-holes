a49edecbcbcab08eae9fa287b4d6edbab44c99ec
vivian menard <menardvivian@gmail.com>
2024-12-28 19:35:58 +0100

    remove exception potential raising when accessing above/below attribute of monotone vertices

--- objects/monotone_vertex.py
+++ objects/monotone_vertex.py
@@ -3,16 +3,14 @@ from __future__ import annotations
 from functools import cached_property
 from typing import TYPE_CHECKING
 
-from exceptions import MonotonyRuleNotRespected, NonExistingAttribute
-
 if TYPE_CHECKING:
     from objects import Vertex
 
 
 class MonotoneVertex:
     vertex: Vertex
-    _above: MonotoneVertex | None
-    _below: MonotoneVertex | None
+    above: MonotoneVertex | None
+    below: MonotoneVertex | None
 
     def __init__(
         self,
@@ -22,39 +20,9 @@ class MonotoneVertex:
     ) -> None:
         self.vertex = vertex
 
-        self._above = None
-        self._below = None
         self.above = above
         self.below = below
 
-    @property
-    def above(self) -> MonotoneVertex:
-        if not self._above:
-            raise NonExistingAttribute
-
-        return self._above
-
-    @property
-    def below(self) -> MonotoneVertex:
-        if not self._below:
-            raise NonExistingAttribute
-
-        return self._below
-
-    @above.setter
-    def above(self, new_above: MonotoneVertex | None) -> None:
-        if new_above is not None and self.vertex > new_above.vertex:
-            raise MonotonyRuleNotRespected
-
-        self._above = new_above
-
-    @below.setter
-    def below(self, new_below: MonotoneVertex | None) -> None:
-        if new_below is not None and self.vertex < new_below.vertex:
-            raise MonotonyRuleNotRespected
-
-        self._below = new_below
-
     @cached_property
     def is_base_vertex(self):
-        return self._above is None or self._below is None
+        return self.above is None or self.below is None
