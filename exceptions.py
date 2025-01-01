08f6a767c36a7bd3eaf6d7c2343286991a3a6f4c
vivian menard <menardvivian@gmail.com>
2025-01-01 15:17:25 +0100

    add docstrings to exceptions.py (#6)

--- exceptions.py
+++ exceptions.py
@@ -1,30 +1,61 @@
 class NonExistingAttribute(Exception):
+    """
+    Raised when attempting to access an attribute that does not exist.
+    """
+
     pass
 
 
 class InconsistentTrapezoidNeighborhood(Exception):
+    """
+    Raised when there is a consistency issue between trapezoids.
+
+    This occurs when the `trapezoids_above` or `trapezoids_below` attributes
+    are not consistent across connected trapezoids.
+    """
+
     pass
 
 
 class BadVertexOrder(Exception):
-    pass
+    """
+    Raised when attempting to create a triangle from three vertices
+    that are not in counter-clockwise order.
+    """
 
-
-class MonotonyRuleNotRespected(Exception):
     pass
 
 
 class NotATrapezoid(Exception):
+    """
+    Raised when attempting to call a trapezoid-specific method on a node
+    that does not represent a trapezoid.
+    """
+
     pass
 
 
 class InconsistentArguments(Exception):
+    """
+    Raised when a method is called with arguments that are logically inconsistent
+    or contradictory.
+    """
+
     pass
 
 
 class NonExistingExtremePoint(Exception):
+    """
+    Raised when attempting to access the extreme point of a trapezoid
+    in a direction where it extends infinitely.
+    """
+
     pass
 
 
 class InconsistentTrapezoid(Exception):
+    """
+    Raised when there is a structural inconsistency in a trapezoid.
+    """
+
     pass
