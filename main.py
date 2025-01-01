3122dbe61c36249040bb42c95dd318309ecfa67b
vivian menard <menardvivian@gmail.com>
2025-01-01 16:11:07 +0100

    add docstring on main (#6)

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from utils import expand_app, set_process_dpi
 
 
 def main():
+    """
+    The main entry point of the application.
+
+    This function sets up the graphical environment, initializes the main drawing Tkinter interface for the
+    polygonal area, and starts the main event loop.
+    """
     set_process_dpi()
 
     root = tk.Tk()
